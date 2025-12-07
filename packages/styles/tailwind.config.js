6965795e29a197ca296185c3ec58f7be9cbeed92
Jörn Seidel <joern.seidel@casoon.de>
2025-12-07 09:30:47 +0100

    Tailwind config: require() durch ESM import ersetzt

--- packages/styles/tailwind.config.js
+++ packages/styles/tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
@@ -87,5 +89,5 @@ export default {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [typography],
 };
